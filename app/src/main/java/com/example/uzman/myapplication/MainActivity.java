d45941f7a9fe56cabd7e3d556a6a7198d5e837ed
Tamer <tamerkaralurt@gmail.com>
2017-11-08 14:47:29 +0300

    kodlar yazıldı ancak hatalar düzenlenecek

--- app/src/main/java/com/example/uzman/myapplication/MainActivity.java
+++ app/src/main/java/com/example/uzman/myapplication/MainActivity.java
@@ -1,13 +1,166 @@
 package com.example.uzman.myapplication;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
+    private int sonuc,total;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = (TextView) findViewById(R.id.textView2);
+
+
+        Button btn_0= (Button) findViewById(R.id.btn_0);
+        btn_0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.append("0");
+                sonuc += Double.parseDouble(text.getText().toString());
+            }
+        });
+
+        Button btn_1= (Button) findViewById(R.id.btn_1);
+        btn_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.append("1");
+            }
+        });
+
+        Button btn_2= (Button) findViewById(R.id.btn_2);
+        btn_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.append("2");
+            }
+        });
+
+        Button btn_3= (Button) findViewById(R.id.btn_3);
+        btn_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.append("3");
+            }
+        });
+
+        Button btn_4= (Button) findViewById(R.id.btn_4);
+        btn_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.append("4");
+            }
+        });
+
+        Button btn_6= (Button) findViewById(R.id.btn_6);
+        btn_6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.append("5");
+            }
+        });
+
+        Button btn_7= (Button) findViewById(R.id.btn_7);
+        btn_7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.append("5");
+            }
+        });
+
+        Button btn_8= (Button) findViewById(R.id.btn_8);
+        btn_8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.append("5");
+            }
+        });
+
+        Button btn_9= (Button) findViewById(R.id.btn_9);
+        btn_9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.append("5");
+            }
+        });
+
+        Button btn_5= (Button) findViewById(R.id.btn_5);
+        btn_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.append("5");
+            }
+        });
+
+        Button btn_plus= (Button) findViewById(R.id.btn_plus);
+        btn_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sonuc = Integer.parseInt(text.getText().toString());
+                text.setText("");
+            }
+        });
+
+
+        Button btn_equal= (Button) findViewById(R.id.btn_equal);
+        btn_equal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(text.getText().toString() != ""){
+                    total = sonuc + Integer.parseInt(text.getText().toString());
+                    text.setText("");
+                    text.setText(String.valueOf(total));
+                }
+            }
+        });
+
+        Button btn_mines= (Button) findViewById(R.id.btn_mines);
+        btn_mines.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                total = sonuc - Integer.parseInt(text.getText().toString());
+                text.setText("");
+                text.setText(String.valueOf(total));
+            }
+        });
+
+        Button btn_multi= (Button) findViewById(R.id.btn_multi);
+        btn_multi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                total = sonuc * Integer.parseInt(text.getText().toString());
+                text.setText("");
+                text.setText(String.valueOf(total));
+            }
+        });
+
+        Button btn_over= (Button) findViewById(R.id.btn_over);
+        btn_over.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                total = sonuc / Integer.parseInt(text.getText().toString());
+                text.setText("");
+                text.setText(String.valueOf(total));
+            }
+        });
+
+
+        Button btn_ac= (Button) findViewById(R.id.btn_ac);
+        btn_ac.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (text.getText().toString() != "")
+                    text.setText("");
+            }
+        });
     }
 }
